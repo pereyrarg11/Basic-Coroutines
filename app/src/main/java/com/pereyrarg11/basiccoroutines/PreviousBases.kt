1b1cc894b58fa1dff33564fef772d88e998ec34c
Gabriel Pereyra R <gabriel@underdog.mx>
2023-03-19 23:33:31 -0600

    chore: example of lambda

--- app/src/main/java/com/pereyrarg11/basiccoroutines/PreviousBases.kt
+++ app/src/main/java/com/pereyrarg11/basiccoroutines/PreviousBases.kt
@@ -1,5 +1,20 @@
 package com.pereyrarg11.basiccoroutines
 
 fun main() {
-    println("Hello world")
+    lambda()
+}
+
+fun lambda() {
+    println(times(5, 8))
+    timesAsLambda(5, 8) { result ->
+        println(result)
+    }
+}
+
+fun timesAsLambda(a: Int, b: Int, callback: (result: Int) -> Unit) {
+    callback(a * b)
+}
+
+fun times(a: Int, b: Int): Int {
+    return a * b
 }
