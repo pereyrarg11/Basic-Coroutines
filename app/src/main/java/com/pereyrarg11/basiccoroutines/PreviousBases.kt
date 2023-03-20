09dd4d8b9cbb87934141c6a5b0d1cc4973ba66c2
Gabriel Pereyra R <gabriel@underdog.mx>
2023-03-19 23:43:49 -0600

    chore: example of threads

--- app/src/main/java/com/pereyrarg11/basiccoroutines/PreviousBases.kt
+++ app/src/main/java/com/pereyrarg11/basiccoroutines/PreviousBases.kt
@@ -1,8 +1,30 @@
 package com.pereyrarg11.basiccoroutines
 
+import kotlin.concurrent.thread
+import kotlin.random.Random.Default.nextLong
+
 fun main() {
     lambda()
+    threads()
 }
+
+fun threads() {
+    println(timesAsThread(5, 8))
+}
+
+fun timesAsThread(a: Int, b: Int): Int {
+    var result = 0
+
+    thread {
+        Thread.sleep(randomSleep())
+        result = a * b
+    }
+
+    Thread.sleep(2_100)
+    return result
+}
+
+fun randomSleep(): Long = nextLong(500, 2_000)
 
 fun lambda() {
     println(times(5, 8))
