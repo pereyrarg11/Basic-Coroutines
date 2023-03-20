67baf8d6966a4e6f8a4a6c34907ff645666b991c
Gabriel Pereyra R <gabriel@underdog.mx>
2023-03-19 23:50:53 -0600

    chore: example of thread with lambda

--- app/src/main/java/com/pereyrarg11/basiccoroutines/PreviousBases.kt
+++ app/src/main/java/com/pereyrarg11/basiccoroutines/PreviousBases.kt
@@ -9,7 +9,10 @@ fun main() {
 }
 
 fun threads() {
-    println(timesAsThread(5, 8))
+    println("timesAsThread: ${timesAsThread(5, 8)}")
+    timesWithThreadAndLambda(5, 8) {
+        println("timesWithThreadAndLambda: $it")
+    }
 }
 
 fun timesAsThread(a: Int, b: Int): Int {
@@ -22,6 +25,16 @@ fun timesAsThread(a: Int, b: Int): Int {
 
     Thread.sleep(2_100)
     return result
+}
+
+fun timesWithThreadAndLambda(a: Int, b: Int, callback: (result: Int) -> Unit) {
+    var result = 0
+
+    thread {
+        Thread.sleep(randomSleep())
+        result = a * b
+        callback(result)
+    }
 }
 
 fun randomSleep(): Long = nextLong(500, 2_000)
