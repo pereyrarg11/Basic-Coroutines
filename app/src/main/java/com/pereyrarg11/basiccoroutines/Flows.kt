b470247377caf3eaeadef8772afb02a6f8f46f96
Gabriel Pereyra R <pereyrarg11@gmail.com>
2023-04-04 19:53:26 -0600

    chore: example of how to cancel a flow

--- app/src/main/java/com/pereyrarg11/basiccoroutines/Flows.kt
+++ app/src/main/java/com/pereyrarg11/basiccoroutines/Flows.kt
@@ -1,10 +1,25 @@
 package com.pereyrarg11.basiccoroutines
 
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() {
-    coldFlow()
+    //coldFlow()
+    cancelFlow()
+}
+
+fun cancelFlow() {
+    runBlocking {
+        newTopic("Cancel flow")
+        val job = launch {
+            getDataByFlow().collect {
+                println(it)
+            }
+        }
+        delay(randomSleep() * 2)
+        job.cancel()
+    }
 }
 
 fun coldFlow() {
